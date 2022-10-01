43a2f78a3ac4e43cb0618c9c35a174380c6267f8
Sahil <Sahil@dotcode.com>
2022-10-01 09:07:37 +0600

    javascriptV-0.2

--- javascript.js
+++ javascript.js
@@ -1,7 +1,63 @@
-let name = 22
-const num = "Sahil"
-{
-let name = 222
-console.log(name)
-}
-console.log(typeof(name + num))
+// let name = 22
+// const num = "Sahil"
+// {
+// let name = 222
+// console.log(name)
+// }
+// console.log(typeof(name + num))
+
+
+// const a = null
+// const b = "name"
+// const c = 111
+// const d = true 
+// const e = BigInt('3')
+// const f = Symbol("dhk")
+// const g = undefined
+
+
+//problem chapter 2 number 1
+// let age = prompt("What's Your Age : ");
+// age = Number.parseInt(age);
+// if(age>10 && age<20){
+//     console.log("Your age is under 10-20.");
+// }else{
+//     console.log("Your age is not under 10-20.");
+// }
+
+// switch (age) {
+//     case 12:
+//         console.log('your age is 12')
+//         break;
+//     case 15:
+//         console.log('your age is 15')
+//         break;
+//     default:
+//         console.log('your age is not special.')
+//         break;
+// }
+
+// if(age%2 == 0 && age%3 == 0){
+//     console.log("age is disival by 2 and 3")
+// }else{
+//     console.log("age is not disival by 2 and 3")
+// }
+// if(age%2==0 || age%3==0){
+//     console.log("age is disival by 2 and 3")
+// }else{
+//     console.log("age is not disival by 2 and 3")
+// }
+
+// let a = age>=18?"you can drive":"sorry";
+// console.log(a)
+
+
+
+
+
+
+
+
+
+
+
